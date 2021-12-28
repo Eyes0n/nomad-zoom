4809d50d59899bb4cffd9e24a581531c4271915a
hyunchanpark <eye3570@gmail.com>
2021-12-28 16:03:18 +0900

    Feat : server 예외 라우팅 처리 redirect 추가

--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
+
+// routes
 app.use('/public', express.static(__dirname + '/public'));
 
+// api
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 app.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
