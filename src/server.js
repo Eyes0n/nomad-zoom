d9e774900bd0e0d7f4029765ab83a668203ea432
hyunchanpark <eye3570@gmail.com>
2021-12-28 19:00:14 +0900

    Feat : WebSocket 추가

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -9,9 +11,12 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 // api
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+app.get('/', (_req, res) => res.render('home'));
+app.get('/*', (_req, res) => res.redirect('/'));
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
 });
