5ab14cf1c202a15b576dec13fb58ab4cada8dbf5
hyunchanpark <eye3570@gmail.com>
2021-12-30 11:58:26 +0900

    Feat : WebSocket event 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert('hello');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.onopen = () => {
+  console.log('WebSocket connection established');
+};
+
+socket.onmessage = (msg) => {
+  console.log('New message : ', msg.data, ' from the server');
+};
+
+socket.onclose = () => {
+  console.log('WebSocket connection closed');
+};
+
+setTimeout(() => {
+  socket.send('hello from the browser');
+}, 3000);
--- src/server.js
+++ src/server.js
@@ -17,6 +17,21 @@ app.get('/*', (_req, res) => res.redirect('/'));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser');
+
+  socket.send('hello!!');
+
+  socket.on('message', (msg) => {
+    // console.log('msg: %s', msg);
+    console.log(`msg: ${JSON.stringify(msg)}`);
+  });
+
+  socket.on('close', () => {
+    console.log('Disconnected from the Browser');
+  });
+});
+
 server.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
 });
