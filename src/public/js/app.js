5440c7ecba37d0eee6f7abbe140b5337436c6ded
hyunchanpark <eye3570@gmail.com>
2022-01-01 15:53:22 +0900

    Feat : socket.io Broadcasting to room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,45 +1,40 @@
-const msgList = document.querySelector('ul');
-const nickForm = document.querySelector('#nick');
-const msgForm = document.querySelector('#message');
+const socket = io();
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById('welcome');
+const roomForm = welcome.querySelector('form');
+const roomMsg = document.getElementById('roomMsg');
 
-socket.onopen = () => {
-  console.log('WebSocket connection established');
-};
+roomMsg.hidden = true;
 
-socket.onmessage = (msg) => {
-  // msg가 blob type으로 들어와서 string으로 변환하기 위해 promise인 blob.text() 사용
-  // 위 방법 이외에 FileReader 객체로 읽는 방법 || 서버(server.js)에서 msg.toString()값을 send하는 방법 등.
-  // console.log('New message :', await msg.data.text());
-  console.log('New message :', msg.data);
+let roomName;
+
+function addMessage(message) {
+  const ul = roomMsg.querySelector('ul');
   const li = document.createElement('li');
-  li.innerText = msg.data;
-  msgList.appendChild(li);
-};
 
-socket.onclose = () => {
-  console.log('WebSocket connection closed');
-};
-
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+  li.innerText = message;
+  ul.appendChild(li);
 }
 
-function handleSubmit(evt) {
+function showRoomMsg() {
+  welcome.hidden = true;
+  roomMsg.hidden = false;
+  const h3 = roomMsg.querySelector('h3');
+  h3.textContent = `Room ${roomName}`;
+}
+
+function handleRoomSubmit(evt) {
   evt.preventDefault();
-  const input = msgForm.querySelector('input');
-  socket.send(makeMessage('message', input.value));
+  const input = roomForm.querySelector('input');
+
+  // send socket message
+  socket.emit('enter_room', input.value, showRoomMsg);
+  roomName = input.value;
   input.value = '';
 }
 
-function handleNickSubmit(evt) {
-  evt.preventDefault();
-  const input = nickForm.querySelector('input');
-  socket.send(makeMessage('nickname', input.value));
-  input.value = '';
-}
+roomForm.addEventListener('submit', handleRoomSubmit);
 
-msgForm.addEventListener('submit', handleSubmit);
-nickForm.addEventListener('submit', handleNickSubmit);
+socket.on('welcome', () => {
+  addMessage('someone joined!');
+});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http';
-import WebSocket from 'ws';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -14,51 +14,25 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_req, res) => res.render('home'));
 app.get('/*', (_req, res) => res.redirect('/'));
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
 
-// Broadcast 1: 배열로 client 관리
-const sockets = [];
+// Socket.Io
+const io = new Server(httpServer);
 
-wss.on('connection', (socket) => {
+io.on('connection', (socket) => {
   console.log('Connected to Browser');
-  // Broadcast 1
-  sockets.push(socket);
 
-  // 임의로 유저 닉네임 설정
-  socket['nickname'] = 'Anon';
+  socket.on('enter_room', (roomName, done) => {
+    // join room
+    socket.join(roomName);
 
-  socket.on('message', (msg) => {
-    console.log('msg: %s', msg);
-    const parsedMsg = JSON.parse(msg);
+    done();
 
-    switch (parsedMsg.type) {
-      case 'message':
-        // Broadcast 1
-        sockets.forEach((socketClient) =>
-          socketClient.send(`${socket.nickname}: ${parsedMsg.payload}`)
-        );
-        // Broadcast 2: websocket.clients 사용
-        // wss.clients.forEach((socketClient) =>
-        //   socketClient.send(`${socket.nickname}: ${parsedMsg.payload}`)
-        // );
-        break;
-
-      case 'nickname':
-        // 닉네임 설정
-        socket['nickname'] = parsedMsg.payload;
-        break;
-
-      default:
-        break;
-    }
-  });
-
-  socket.on('close', () => {
-    console.log('Disconnected from the Browser');
+    // Broadcasting to others in room
+    socket.to(roomName).emit('welcome');
   });
 });
 
-server.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
 });
